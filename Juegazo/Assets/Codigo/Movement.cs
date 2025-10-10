645bab892e29ece9a6826e91942e03341feae40f
IESPIRINGALLA\ferlopdie <diegoferreiros03@gmail.com>
2025-10-10 18:17:14 +0200

    Mejoras

--- Juegazo/Assets/Codigo/Movement.cs
+++ Juegazo/Assets/Codigo/Movement.cs
@@ -2,14 +2,108 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Rigidbody2D rb;
+    //[SerializeField] EdgeCollider2D coll;
+    [SerializeField] CapsuleCollider2D coll;
+    [SerializeField] LayerMask jumpableGround;
+    [SerializeField] int saltos = 0;
+
+    [SerializeField] int vidas = 3;
+
+    enum AnimationType { idle, running, jumping }
+    AnimationType state = AnimationType.idle;
+    SpriteRenderer sr;
+
+    Animator animator;
+
     void Start()
     {
-        
+        coll = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+
+        state = AnimationType.idle;
+
+        if (Keyboard.current.dKey.isPressed())
+        {
+            state = AnimationType.running;
+            sr.flipX = false;
+            if (rb.linearVelocityX <= 5)
+            {
+                rb.linearVelocityX += 0.2f;
+            }
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            state = AnimationType.running;
+            sr.flipX = true;
+            if (rb.linearVelocityX >= -5)
+            {
+                rb.linearVelocityX -= 0.2f;
+            }
+        }
+
+        // Frenar de vez
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            rb.linearVelocityX = 0f;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGround())
+        {
+            state = AnimationType.jumping;
+            rb.linearVelocityY = 5;
+            saltos = 1;
+        }
+
+        if (saltos == 1 && !isGround())
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                state = AnimationType.jumping;
+                rb.linearVelocityY += 7;
+                saltos = 2;
+            }
+        }
+
+        if (saltos == 2)
+        {
+            saltos = 0;
+        }
+
+        animator.SetInteger("estado", (int)state);
+
+    }
+
+
+    private bool isGround() => Physics2D.BoxCast(
+        coll.bounds.center,
+        coll.bounds.size,
+        0f,
+        Vector2.down,
+        .1f,
+        jumpableGround
+    );
+
+    private void ReiniciarJuego()
+    {
+        vidas--;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Muerte"))
+        {
+            ReiniciarJuego();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
